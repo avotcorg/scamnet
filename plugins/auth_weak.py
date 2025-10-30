01729ad18e62e535d143f2e4685140d4f4223a86
avotcorg <102286982+avotcorg@users.noreply.github.com>
2025-10-30 16:54:51 +0800

    Update auth_weak.py

--- plugins/auth_weak.py
+++ plugins/auth_weak.py
@@ -3,7 +3,7 @@ WEAK_PASSWORDS = [
     "1234:1234", "admin:admin", "socks5:socks5", "123456:123456",
     "12345678:12345678", "admin123:admin", "proxy:proxy", "admin:123456", "root:root",
     "12345:12345", "test:test", "user:user", "guest:guest", "admin:", "888888:888888", 
-  "test123:test123", "qwe:qwe", "qwer:qwer", "qwer:qwer", "11:11", "222:222", "2:2", "3:3",
+  "test123:test123", "qwe:qwe", "11:11", "222:222", "2:2", "3:3",
   "12349:12349", "12349:12349", "user:123", "user:1234", "user:12345", "user:123456"
 ]
 
