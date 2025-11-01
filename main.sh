b6f71bcf03142d17d5b4cdaeed5da7f40a2cdd67
avotcorg <102286982+avotcorg@users.noreply.github.com>
2025-11-01 12:06:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# main.sh - Scamnet OTC v1.3 Scamnet Go v1.3 OTC TG:soqunla （日志极致优化 + 自动轮转）
+# main.sh - Scamnet Go v1.3 OTC TG:soqunla （日志极致优化 + 自动轮转）
 set -euo pipefail
 IFS=$'\n\t'
 RED='\033[31m'; GREEN='\033[32m'; YELLOW='\033[33m'; BLUE='\033[34m'; NC='\033[0m'
